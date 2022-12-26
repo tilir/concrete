6e47804bad1c760f9f0e203008f8fa3ed7d1a86e
Tilir <konstantin.vladimirov@gmail.com>
2022-12-26 17:39:09 +0300

    Adding simple app to read permutaton and output a tree

---
+++ apps/readbst.cc
@@ -0,0 +1,72 @@
+//------------------------------------------------------------------------------
+//
+// Read permutation from stdin and create bst
+// Permutation format: N a1 .. aN
+// Dump resulting bst to stdout (plenty of options)
+//
+//------------------------------------------------------------------------------
+//
+// This file is licensed after LGPL v3
+// Look at: https://www.gnu.org/licenses/lgpl-3.0.en.html for details
+//
+//------------------------------------------------------------------------------
+
+#include "opts.hpp"
+#include "tabtree.hpp"
+#include <fstream>
+#include <iostream>
+
+namespace {
+
+constexpr bool DEF_VERBOSE = false;
+constexpr bool DEF_DOT = false;
+constexpr bool DEF_BACK = false;
+
+struct Config {
+  bool Verbose = DEF_VERBOSE;
+  bool DumpDot = DEF_DOT;
+  bool Back = DEF_BACK;
+};
+
+Config parse_cfg(int argc, char **argv) {
+  Config Cfg;
+  options::Parser OptParser;
+  OptParser.template add<int>("verbose", DEF_VERBOSE, "a lot of debug output");
+  OptParser.template add<int>("dot", DEF_DOT, "output dot file");
+  OptParser.template add<int>("back", DEF_BACK, "read input backwards");
+  OptParser.parse(argc, argv);
+
+  Cfg.Verbose = OptParser.exists("verbose");
+  Cfg.DumpDot = OptParser.exists("dot");
+  Cfg.Back = OptParser.exists("back");
+  return Cfg;
+}
+
+template <typename T>
+trees::TabTree<T> read_bst_ordered(std::istream &Is, const Config &Cfg) {
+  int N;
+  Is >> N;
+  std::vector<T> Vec(N);
+  for (int I = 0; I < N; ++I)
+    Is >> Vec[I];
+
+  if (Cfg.Back)
+    std::reverse(Vec.begin(), Vec.end());
+
+  trees::TabTree<T> Ret(N);
+  for (int I = 0; I < N; ++I)
+    Ret.addSearchOrder(Vec[I]);
+  return Ret;
+}
+
+} // namespace
+
+int main(int argc, char **argv) {
+  auto Cfg = parse_cfg(argc, argv);
+  std::cin.exceptions(std::ios_base::failbit);
+  auto Tree = read_bst_ordered<int>(std::cin, Cfg);
+  if (Cfg.DumpDot)
+    Tree.dumpDot(std::cout);
+  else
+    Tree.dumpEL(std::cout);
+}
---
+++ trees/tabtree.hpp
@@ -0,0 +1,137 @@
+//------------------------------------------------------------------------------
+//
+// Tabulated tree: binary tree with left and rights vectors
+// optimized for almost immutable trees
+//
+//------------------------------------------------------------------------------
+//
+// This file is licensed after LGPL v3
+// Look at: https://www.gnu.org/licenses/lgpl-3.0.en.html for details
+//
+//------------------------------------------------------------------------------
+
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <vector>
+
+namespace trees {
+
+template <typename T> class TabTree {
+  std::vector<int> Left, Right;
+  std::vector<T> Data;
+  int Root;
+  int FreePos = 0;
+
+  // Pos is right or left link in parent
+  template <typename It> void addNode(It Pos, T Dt) {
+    assert(FreePos < Data.size());
+    *Pos = FreePos;
+    Data[FreePos] = std::move(Dt);
+    FreePos += 1;
+  }
+
+  void addSearchOrderRec(int N, T Dt) {
+    if (Dt < Data[N]) {
+      if (Left[N] != -1)
+        addSearchOrderRec(Left[N], std::move(Dt));
+      else
+        addNode(Left.begin() + N, std::move(Dt));
+    } else {
+      assert(Dt != Data[N]);
+      if (Right[N] != -1)
+        addSearchOrderRec(Right[N], std::move(Dt));
+      else
+        addNode(Right.begin() + N, std::move(Dt));
+    }
+  }
+
+  template <typename It> void setRanks(It Ranks, int N) const {
+    if (Left[N] != -1) {
+      Ranks[Left[N]] = Ranks[N] + 1;
+      setRanks(Ranks, Left[N]);
+    }
+    if (Right[N] != -1) {
+      Ranks[Right[N]] = Ranks[N] + 1;
+      setRanks(Ranks, Right[N]);
+    }
+  }
+
+public:
+  explicit TabTree(int Sz, int Rt = -1)
+      : Left(Sz, -1), Right(Sz, -1), Data(Sz), Root(Rt) {}
+
+  void addSearchOrder(T Dt) {
+    // if Root is -1 this is first node, add to 0 and set Root to 0.
+    if (Root == -1) {
+      assert(FreePos == 0);
+      Data[0] = std::move(Dt);
+      Root = 0;
+      FreePos += 1;
+      return;
+    }
+
+    addSearchOrderRec(Root, std::move(Dt));
+  }
+
+  // dump tree as edge list
+  void dumpEL(std::ostream &os) const {
+    for (int I = 0; I < Data.size(); ++I) {
+      if (Left[I] != -1)
+        os << "v" << Data[I] << " -- "
+           << "v" << Data[Left[I]] << "\n";
+      if (Right[I] != -1)
+        os << "v" << Data[I] << " -- "
+           << "v" << Data[Right[I]] << "\n";
+    }
+  }
+
+  void dumpDot(std::ostream &os) const {
+    os << "graph {\n";
+    if (Root == -1) {
+      os << "}\n";
+      return;
+    }
+
+    // pretty tree: determine same-rank elements
+    assert(Root == 0);
+    std::vector<int> Ranks(Data.size(), -1);
+    Ranks[Root] = 0;
+    os << "{ rank=source " << Data[Root] << "}\n";
+    os << "{ rank=sink nil }\n";
+    setRanks(Ranks.begin(), 0);
+    std::multimap<int, int> MRanks;
+    for (int I = 0; I < Data.size(); ++I)
+      MRanks.insert({Ranks[I], I});
+    auto MaxRankIt = std::max_element(Ranks.begin(), Ranks.end());
+    if (MaxRankIt != Ranks.end())
+      for (int I = 1; I < *MaxRankIt; ++I) {
+        auto [It, Ite] = MRanks.equal_range(I);
+        if (It != Ite) {
+          os << "{ rank=same ";
+          for (auto EltIt = It; EltIt != Ite; ++EltIt)
+            os << Data[EltIt->second] << " ";
+          os << " }\n";
+        }
+      }
+
+    // like dumpEL but with nil
+    for (int I = 0; I < Data.size(); ++I) {
+      os << Data[I] << " -- ";
+      if (Left[I] != -1)
+        os << Data[Left[I]] << " [color = red]\n";
+      else
+        os << "nil [style=dotted]\n";
+
+      os << Data[I] << " -- ";
+      if (Right[I] != -1)
+        os << Data[Right[I]] << " [color = blue]\n";
+      else
+        os << "nil [style=dotted]\n";
+    }
+
+    os << "}\n";
+  }
+};
+
+} // namespace trees
