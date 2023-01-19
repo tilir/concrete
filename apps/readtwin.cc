a1ec4ae532cf700243ebc0266e879c33b59774f1
Tilir <konstantin.vladimirov@gmail.com>
2023-01-20 01:16:32 +0300

    More tests for lecture
    Examples for lecture slides

--- apps/readtwin.cc
+++ apps/readtwin.cc
@@ -73,7 +73,7 @@ bool BatchStep(int N, std::istream &Is, std::ostream &Os, const Config &Cfg) {
 
   if (Cfg.Baxters) {
     std::ostream_iterator<int> OsIt(std::cout, " ");
-    auto B = Tree->toBaxters();
+    auto B = Tree->toBaxters(Cfg.Verbose);
     if (!Cfg.Batch)
       std::cout << N << std::endl;
     ranges::copy(B, OsIt);
